744550beb79e56d7db995a58e05f6665759bc652
destruxo23 <81651463+destruxo23@users.noreply.github.com>
2024-10-09 19:40:04 -0300

    Busqueda de datos en una matriz

---
+++ Proyecto_Once/Proyecto_Once/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Proyecto_Once
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnConsultar = new Button();
+            txtPrecio = new TextBox();
+            txtNombre = new TextBox();
+            txtCodigo = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // btnConsultar
+            // 
+            btnConsultar.Location = new Point(30, 234);
+            btnConsultar.Name = "btnConsultar";
+            btnConsultar.Size = new Size(75, 23);
+            btnConsultar.TabIndex = 0;
+            btnConsultar.Text = "Consultar";
+            btnConsultar.UseVisualStyleBackColor = true;
+            btnConsultar.Click += btnConsultar_Click;
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(30, 192);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(169, 23);
+            txtPrecio.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(30, 130);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(233, 23);
+            txtNombre.TabIndex = 2;
+            // 
+            // txtCodigo
+            // 
+            txtCodigo.Location = new Point(30, 74);
+            txtCodigo.Name = "txtCodigo";
+            txtCodigo.Size = new Size(233, 23);
+            txtCodigo.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(30, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Codigo";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(30, 112);
+            label2.Name = "label2";
+            label2.Size = new Size(103, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Nombre producto";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(30, 174);
+            label3.Name = "label3";
+            label3.Size = new Size(40, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Precio";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(329, 331);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtCodigo);
+            Controls.Add(txtNombre);
+            Controls.Add(txtPrecio);
+            Controls.Add(btnConsultar);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnConsultar;
+        private TextBox txtPrecio;
+        private TextBox txtNombre;
+        private TextBox txtCodigo;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
