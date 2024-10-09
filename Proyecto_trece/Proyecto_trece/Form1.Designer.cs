2ca139150a1bec9c936aac8ee97fd9985675ee78
destruxo23 <81651463+destruxo23@users.noreply.github.com>
2024-10-09 19:56:51 -0300

    Parametros y argumentos en funciones

---
+++ Proyecto_trece/Proyecto_trece/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Proyecto_trece
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtNumero1 = new TextBox();
+            txtNumero2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            btnCalcular = new Button();
+            SuspendLayout();
+            // 
+            // txtNumero1
+            // 
+            txtNumero1.Location = new Point(77, 70);
+            txtNumero1.Name = "txtNumero1";
+            txtNumero1.Size = new Size(100, 23);
+            txtNumero1.TabIndex = 0;
+            // 
+            // txtNumero2
+            // 
+            txtNumero2.Location = new Point(77, 131);
+            txtNumero2.Name = "txtNumero2";
+            txtNumero2.Size = new Size(100, 23);
+            txtNumero2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(77, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Numero 1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(77, 113);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Numero 2";
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Location = new Point(90, 181);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(75, 27);
+            btnCalcular.TabIndex = 4;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(263, 261);
+            Controls.Add(btnCalcular);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtNumero2);
+            Controls.Add(txtNumero1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtNumero1;
+        private TextBox txtNumero2;
+        private Label label1;
+        private Label label2;
+        private Button btnCalcular;
+    }
+}
